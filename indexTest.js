59a664caa527ee70103bce1e7b9cc0325153dcbd
Bala Khan <118004792+BalaQan@users.noreply.github.com>
2024-06-05 18:19:14 +0200

    Create indexTest.js

---
+++ indexTest.js
@@ -0,0 +1,49 @@
+/** @type {import('next').NextConfig} */
+const defaultNextConfig = {
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+  swcMinify: false,
+  poweredByHeader: false,
+  staticPageGenerationTimeout: 180,
+  experimental: {
+    esmExternals: 'loose',
+  },
+  images: {
+    loader: '',
+    path: 'https:',
+    domains: [],
+  },
+  eslint: {
+    dirs: [
+      // ...
+      'app',
+      'components',
+      'functions',
+      'lib',
+      'pages',
+      'providers',
+      'types',
+      'ui',
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // If client-side, don't polyfill `fs`
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        dns: false,
+        tls: false,
+        net: false,
+      }
+    }
+
+    config.module = {
+      ...config.module,
+      exprContextCritical: false,
+    }
+
+    return config
+  },
+}
+
+module.exports = defaultNextConfig
